f5e293e62b973967580203e4dcdea00185f16536
Ritayan Chakraborty <ritayanout@gmail.com>
2017-06-26 19:08:10 +0530

    Add colours

---
+++ app/src/main/java/gh/out386/timer/Constants.java
@@ -0,0 +1,7 @@
+package gh.out386.timer;
+
+class Constants {
+    static String colourPrimary = "colourPrimary";
+    static String colourAccent = "colourAccent";
+    static String initialTime = "initialTime";
+}
--- app/src/main/java/gh/out386/timer/MainActivity.java
+++ app/src/main/java/gh/out386/timer/MainActivity.java
@@ -1,61 +1,84 @@
 package gh.out386.timer;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.TextView;
-
-import java.util.Locale;
-import java.util.Timer;
-import java.util.Date;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import java.util.TimerTask;
-import android.os.Bundle;
-import android.os.SystemClock;
 import android.os.Looper;
-import java.text.SimpleDateFormat;
+import android.os.SystemClock;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.afollestad.materialdialogs.color.ColorChooserDialog;
 import com.hanks.htextview.evaporate.EvaporateTextView;
-import android.graphics.Color;
-import gh.out386.timer.FontManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends AppCompatActivity implements ColorChooserDialog.ColorCallback {
     private EvaporateTextView tv;
     private Timer timer;
     private long initialTime;
+    private int colourPrimary;
+    private int colourAccent;
+    private SharedPreferences sp;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-	    tv = (EvaporateTextView) findViewById(R.id.tv);
-        final int [] timerLength = {-1};
-        if (savedInstanceState == null)
+        sp = getPreferences(Context.MODE_PRIVATE);
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.rl);
+        tv = (EvaporateTextView) findViewById(R.id.tv);
+        final int[] timerLength = {-1};
+
+
+        if (savedInstanceState == null) {
             initialTime = SystemClock.elapsedRealtime();
-        else
-            initialTime = savedInstanceState.getLong("initial");
-            
-        tv.setTextColor(Color.BLACK);
+            colourPrimary = sp.getInt(Constants.colourPrimary, 0xffffff);
+            colourAccent = sp.getInt(Constants.colourAccent, 0x000000);
+        } else {
+            initialTime = savedInstanceState.getLong(Constants.initialTime);
+            colourPrimary = savedInstanceState.getInt(Constants.colourPrimary);
+            colourAccent = savedInstanceState.getInt(Constants.colourAccent);
+        }
+
+        container.setBackgroundColor(colourPrimary);
+        tv.setTextColor(colourAccent);
         tv.setTypeface(FontManager.getInstance(getAssets()).getFont("fonts/NotCourierSans-webfont.ttf"));
-        
+
+        container.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                new ColorChooserDialog.Builder(MainActivity.this, R.string.accent)
+                        .accentMode(true)
+                        .show();
+                return true;
+            }
+        });
+
 
         final SimpleDateFormat sdf = new SimpleDateFormat("ss:SS");
         final Date date = new Date();
-	    timer = new Timer();
+        timer = new Timer();
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        
+
         timer.scheduleAtFixedRate(new TimerTask() {
-           @Override
+            @Override
             public void run() {
                 final long diff = SystemClock.elapsedRealtime() - initialTime;
                 if (timerLength[0] == -1 && diff > 50000)
                     timerLength[0] = 1;
                 else if (timerLength[0] != 0 && diff > 2750000)
                     timerLength[0] = 2;
-                    
+
                 if (timerLength[0] == 1) {
                     sdf.applyPattern("mm:ss:SS");
                     timerLength[0] = -2;
@@ -68,7 +91,7 @@ public class MainActivity extends Activity
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
-                        tv.animateText(String.format(Locale.ENGLISH, "%2s", sdf.format(date)));
+                        tv.animateText(sdf.format(date));
                     }
                 });
             }
@@ -76,9 +99,10 @@ public class MainActivity extends Activity
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState)
-    {
-        outState.putLong("initial", initialTime);
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putLong(Constants.initialTime, initialTime);
+        outState.putInt(Constants.colourPrimary, colourPrimary);
+        outState.putInt(Constants.colourAccent, colourAccent);
         super.onSaveInstanceState(outState);
     }
 
@@ -86,5 +110,29 @@ public class MainActivity extends Activity
     public void onDestroy() {
         timer.cancel();
         super.onDestroy();
+    }
+
+    @Override
+    public void onColorSelection(@NonNull ColorChooserDialog dialog, @ColorInt int color) {
+        if (R.string.primary == dialog.getTitle()) {
+            findViewById(R.id.rl).setBackgroundColor(color);
+            colourPrimary = color;
+            sp.edit().putInt(Constants.colourPrimary, color).apply();
+        } else if (R.string.accent == dialog.getTitle()) {
+            tv.setTextColor(color);
+            colourAccent = color;
+            sp.edit().putInt(Constants.colourAccent, color).apply();
+        }
+    }
+
+    @Override
+    public void onColorChooserDismissed(@NonNull ColorChooserDialog dialog) {
+
+        if (dialog.getTitle() == R.string.accent) {
+
+            new ColorChooserDialog.Builder(MainActivity.this, R.string.primary)
+                    .accentMode(false)
+                    .show();
+        }
     }
 }
