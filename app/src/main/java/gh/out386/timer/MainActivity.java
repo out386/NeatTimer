189848262c6422abcddb67f3426d46f0855ffb92
Ritayan Chakraborty <ritayanout@gmail.com>
2017-06-26 18:08:00 +0530

    Use a monospace font

--- app/src/main/java/gh/out386/timer/MainActivity.java
+++ app/src/main/java/gh/out386/timer/MainActivity.java
@@ -4,15 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
-import java.util.*;
-import android.os.*;
-import java.text.*;
-import android.util.*;
+
+import java.util.Locale;
+import java.util.Timer;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import java.util.TimerTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.os.Looper;
+import java.text.SimpleDateFormat;
+import com.hanks.htextview.evaporate.EvaporateTextView;
+import android.graphics.Color;
+import gh.out386.timer.FontManager;
 
 
 public class MainActivity extends Activity 
 {
-    private TextView tv;
+    private EvaporateTextView tv;
     private Timer timer;
     private long initialTime;
 
@@ -21,14 +31,18 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-	    tv = (TextView) findViewById(R.id.tv);
-        final int [] timerLength = new int [1];
+	    tv = (EvaporateTextView) findViewById(R.id.tv);
+        final int [] timerLength = {-1};
         if (savedInstanceState == null)
             initialTime = SystemClock.elapsedRealtime();
         else
             initialTime = savedInstanceState.getLong("initial");
+            
+        tv.setTextColor(Color.BLACK);
+        tv.setTypeface(FontManager.getInstance(getAssets()).getFont("fonts/NotCourierSans-webfont.ttf"));
+        
 
-        final SimpleDateFormat sdf = new SimpleDateFormat("ss : SS");
+        final SimpleDateFormat sdf = new SimpleDateFormat("ss:SS");
         final Date date = new Date();
 	    timer = new Timer();
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
@@ -37,16 +51,16 @@ public class MainActivity extends Activity
            @Override
             public void run() {
                 final long diff = SystemClock.elapsedRealtime() - initialTime;
-                if (diff > 50000)
+                if (timerLength[0] == -1 && diff > 50000)
                     timerLength[0] = 1;
-                else if (diff > 2750000)
+                else if (timerLength[0] != 0 && diff > 2750000)
                     timerLength[0] = 2;
                     
                 if (timerLength[0] == 1) {
-                    sdf.applyPattern("mm : ss : SS");
-                    timerLength[0] = 0;
+                    sdf.applyPattern("mm:ss:SS");
+                    timerLength[0] = -2;
                 } else if (timerLength[0] == 2) {
-                    sdf.applyPattern("HH : mm : ss");
+                    sdf.applyPattern("HH:mm:ss");
                     timerLength[0] = 0;
                 }
                 date.setTime(diff);
@@ -54,11 +68,11 @@ public class MainActivity extends Activity
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
-                        tv.setText(sdf.format(date));
+                        tv.animateText(String.format(Locale.ENGLISH, "%2s", sdf.format(date)));
                     }
                 });
             }
-        }, 95, 95);
+        }, 205, 205);
     }
 
     @Override
