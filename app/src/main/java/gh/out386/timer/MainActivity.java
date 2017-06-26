191352b2d6140a95b8b44b92ad71ab6e12ba2b86
Ritayan Chakraborty <ritayanout@gmail.com>
2017-06-26 13:33:09 +0530

    Handle lifecycle and cancel the timer

--- app/src/main/java/gh/out386/timer/MainActivity.java
+++ app/src/main/java/gh/out386/timer/MainActivity.java
@@ -4,42 +4,73 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import java.util.*;
+import android.os.*;
+import java.text.*;
+import android.util.*;
 
 
 public class MainActivity extends Activity 
 {
     private TextView tv;
-    private int minutes = 0;
-    private int seconds = 0;
+    private Timer timer;
+    private long initialTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 	    tv = (TextView) findViewById(R.id.tv);
-		final Handler handler = new Handler();
-		handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				tick();
-                handler.postDelayed(this, 1000);
-			}
-		}, 1000);
-    }
-    
-    private void tick() {
-        if (seconds < 60)
-            seconds ++;
-        else {
-            seconds = 0;
-            if (minutes < 60)
-                minutes++;
-            else
-                minutes = 0;
-        }
+        final int [] timerLength = new int [1];
+        if (savedInstanceState == null)
+            initialTime = SystemClock.elapsedRealtime();
+        else
+            initialTime = savedInstanceState.getLong("initial");
+
+        final SimpleDateFormat sdf = new SimpleDateFormat("ss : SS");
+        final Date date = new Date();
+	    timer = new Timer();
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         
-        String strMinutes = minutes < 10 ? "0" + minutes : String.valueOf(minutes);
-        String strSeconds = seconds < 10 ? "0" + seconds : String.valueOf(seconds);
-        tv.setText(strMinutes + " : " + strSeconds);
+        timer.scheduleAtFixedRate(new TimerTask() {
+           @Override
+            public void run() {
+                final long diff = SystemClock.elapsedRealtime() - initialTime;
+                if (diff > 50000)
+                    timerLength[0] = 1;
+                else if (diff > 2750000)
+                    timerLength[0] = 2;
+                    
+                if (timerLength[0] == 1) {
+                    sdf.applyPattern("mm : ss : SS");
+                    timerLength[0] = 0;
+                } else if (timerLength[0] == 2) {
+                    sdf.applyPattern("HH : mm : ss");
+                    timerLength[0] = 0;
+                }
+                date.setTime(diff);
+
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(sdf.format(date));
+                    }
+                });
+            }
+        }, 95, 95);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        outState.putLong("initial", initialTime);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
     }
 }
