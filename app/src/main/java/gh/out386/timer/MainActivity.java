466880817d443c35fca4ba00afc69e8ae42bdb3e
Ritayan Chakraborty <ritayanout@gmail.com>
2017-06-27 11:39:15 +0530

    Immersive mode

--- app/src/main/java/gh/out386/timer/MainActivity.java
+++ app/src/main/java/gh/out386/timer/MainActivity.java
@@ -91,6 +91,14 @@ public class MainActivity extends AppCompatActivity implements ColorChooserDialo
         tv.setOnClickListener(this);
         container.setOnClickListener(this);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         container.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
